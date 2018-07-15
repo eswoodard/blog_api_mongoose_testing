d60c8df1668a599784f929a35bde0255a6677663
Elizabeth Woodard <eswoodard75@gmail.com>
2018-07-14 21:26:50 -0400

    all endpoint tests added

--- test/test-blog-app-integration.js
+++ test/test-blog-app-integration.js
@@ -14,7 +14,7 @@ const {TEST_DATABASE_URL} = require('../config');
 chai.use(chaiHttp);
 
 function seedBlogPostData() {
-  console.ing('seeding blog post data');
+  console.info('seeding blog post data');
   const seedData = [];
 
   for (let i=1; i<=10; i++) {
@@ -100,5 +100,86 @@ describe('BlogPost API resource', function (){
     });
   });
 
-})
+
+  describe('POST endpoint', function() {
+    it('should add a new blog post', function() {
+
+      const newBlogPost = generateBlogPostData();
+      
+      return chai.request(app)
+      .post('/posts')
+      .send(newBlogPost)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys(
+          'id', 'title', 'content', 'author', 'publishDate'
+        );
+        expect(res.body.id).to.not.be.null;
+        expect(res.body.title).to.equal(newBlogPost,title);
+        expect(res.body.content).to.equal(newBlogPost.content);
+        expect(res.body.author).to.contain(newBlogPost.author.firstName);
+        expect(res.body.publishDate).to.equal(blog_post.publishDate);
+        return BlogPost.findById(res.body.id);
+      })
+      .then(function(blog_post) {
+        expect(blog_post.title).to.equal(newBlogPost.name);
+        expect(blog_post.content).to.equal(newBlogPost.content);
+        expect(blog_post.author.firstName).to.equal(newBlogPost.author.firstName);
+        expect(blog_post.author.lastName).to.equal(newBlogPost.author.lastName);
+        expect(blog_post.publishDate).to.equal(newBlogPost.publishDate);
+      });
+    });
+  });
+
+  describe('PUT endpoint', function() {
+    it('should update blog post as requested', function() {
+      const updateData = {
+        title: 'blog post',
+        content: 'this is blog post new content',
+        author: {firstName: 'Sally', lastName: 'Student'}
+      };
+
+      return BlogPost
+      .findOne()
+      .then(function(blog_post) {
+        updateData.id = blog_post.id;
+
+        return chai.request(app)
+        .put(`/posts/${blog_post.id}`)
+        .send(updateData);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+        return BlogPost.findById(updateData.id);
+      })
+      .then(function(blog_post) {
+        expect(blog_post.title).to.equal(updateData.title);
+        expect(blog_post.content).to.equal(updateData.content);
+      });
+    });
+  });
+
+  describe('DELETE endpoint', function() {
+    it('should delete a blog post by id', function() {
+      let blog_post;
+
+      return BlogPost
+      .findOne()
+      .then(function(_blog_post) {
+        blog_post = _blog_post;
+        return chai.request(app).deleted(`/posts/${blog_post.id}`);
+      })
+      .then(function(res) {
+        expect(res).to.have.status(204);
+        return BlogPost.findById(blog_post.id);
+      })
+      .then(function(_blog_post) {
+        expect(_blog_post).to.be.null;
+      });
+    });
+  });
+
+});
 
